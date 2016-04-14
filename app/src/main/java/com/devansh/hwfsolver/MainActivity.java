4653cfd6e821805bd1a18ef87490ef518530ebc4
Devansh Gupta <devansh@wearableintelligence.com>
2016-04-14 12:35:27 +0530

    solve implementation

--- app/src/main/java/com/devansh/hwfsolver/MainActivity.java
+++ app/src/main/java/com/devansh/hwfsolver/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 import com.devansh.hwfsolver.com.devansh.dawg.DawgArray;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ProgressDialog progressDialog;
 
-    TextView letters, striked;
+    TextView letters, striked, mostProbableLetter;
     Button solve, findWords;
     ListView list;
 
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         letters = (TextView) findViewById(R.id.letters);
         striked = (TextView) findViewById(R.id.striked);
+        mostProbableLetter = (TextView) findViewById(R.id.mostProbableLetter);
         solve = (Button) findViewById(R.id.solve);
         findWords = (Button) findViewById(R.id.findWords);
         list = (ListView) findViewById(R.id.list);
@@ -50,6 +53,113 @@ public class MainActivity extends AppCompatActivity {
                 findWords(letters.getText().toString());
             }
         });
+
+        solve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solve(letters.getText().toString(), striked.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void solve(String word, String strikedLetters) {
+        strikedLetters = strikedLetters.toLowerCase();
+        final String finalWord = word.toLowerCase();
+        startProgress();
+
+        final boolean[] strikedMap = new boolean[26];
+        for (int i = 0; i < strikedLetters.length(); i++) {
+            strikedMap[strikedLetters.charAt(i) - 'a'] = true;
+        }
+
+
+        new AsyncTask<Void, Void, List<String>>() {
+            String probableText = "";
+
+            private void solveHelper(int currentNode, int currentIndex, StringBuilder prefix,
+                                     List<String> res) {
+
+                if (currentIndex == finalWord.length()) {
+                    if (dawgArray.isEndOFWord(currentNode)) {
+                        res.add(prefix.toString());
+                    }
+                    return;
+                }
+
+                char current = finalWord.charAt(currentIndex);
+
+                for (Map.Entry<Character, Integer> children : dawgArray
+                        .getChildren(currentNode).entrySet()) {
+                    char child = children.getKey();
+                    int childNode = children.getValue();
+
+                    if (current == child || (current == '.' && !strikedMap[child - 'a'])) {
+                        prefix.append(child);
+                        solveHelper(childNode, currentIndex + 1, prefix, res);
+                        prefix.replace(prefix.length() - 1, prefix.length(), "");
+                    }
+
+                }
+            }
+
+            @Override
+            protected List<String> doInBackground(Void... params) {
+                List<String> res = new ArrayList<>();
+                solveHelper(0, 0, new StringBuilder(), res);
+                Collections.sort(res);
+
+                int[] charCounts = new int[26];
+                HashSet<Character> seen = new HashSet<>();
+                for (String word : res) {
+                    int len = word.length();
+                    seen.clear();
+                    for (int i = 0; i < len; i++) {
+                        char c = (char) (word.charAt(i) - 'a');
+                        if (!seen.contains(c)) {
+                            charCounts[c]++;
+                            seen.add(c);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < finalWord.length(); i++) {
+                    if (finalWord.charAt(i) != '.') {
+                        strikedMap[finalWord.charAt(i) - 'a'] = true;
+                    }
+                }
+
+                char bestLetter = 'a';
+                int bestValue = Integer.MIN_VALUE;
+                for (int i = 0; i < charCounts.length; i++) {
+                    if (charCounts[i] > bestValue && !strikedMap[i]) {
+                        bestValue = charCounts[i];
+                        bestLetter = (char) ('a' + i);
+                    }
+                }
+
+                if (bestValue == Integer.MIN_VALUE) {
+                    probableText = "No solutions";
+                } else {
+                    probableText = "Most Probable Letter: " + bestLetter + " (" +
+                            charCounts[bestLetter - 'a'] + "/" + res.size() + ")";
+                }
+
+                return res;
+            }
+
+            @Override
+            protected void onPostExecute(List<String> result) {
+                if (isActivityDestroyed()) {
+                    return;
+                }
+                stopProgress();
+                ((ArrayAdapter) list.getAdapter()).clear();
+                ((ArrayAdapter<String>) list.getAdapter()).addAll(result);
+                mostProbableLetter.setText(probableText);
+            }
+        }.execute();
     }
 
     private void findWords(String letters) {
@@ -59,6 +169,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < letters.length(); i++) {
             chars[letters.charAt(i) - 'a']++;
         }
+        mostProbableLetter.setText("");
 
         new AsyncTask<Void, Void, List<String>>() {
 
@@ -86,6 +197,12 @@ public class MainActivity extends AppCompatActivity {
             protected List<String> doInBackground(Void... params) {
                 List<String> res = new ArrayList<>();
                 findWordsHelper(0, new StringBuilder(), res, chars);
+                Collections.sort(res, new Comparator<String>() {
+                    @Override
+                    public int compare(String lhs, String rhs) {
+                        return rhs.length() - lhs.length();
+                    }
+                });
                 return res;
             }
 
